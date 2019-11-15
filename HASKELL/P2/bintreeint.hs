8ddd46e8936c7ea03979b7546c76f09d66fc4cc5
Aret <aretx100@gmail.com>
2019-11-15 18:38:28 +0100

    Practica completa
    P2 completa

--- HASKELL/P2/bintreeint.hs
+++ HASKELL/P2/bintreeint.hs
@@ -1,2 +1,35 @@
 module BinTreeInt where
 	data BinTreeInt = Void | Node Int BinTreeInt BinTreeInt deriving Show
+
+	-- EJERCICIO 14
+	insTree :: Int -> BinTreeInt -> BinTreeInt
+
+	insTree a (Void) = (Node a Void Void)
+	insTree a (Node num bin1 bin2) 
+		| (a == num) = (Node a bin1 bin2)
+		| (a < num) = (Node num (insTree a bin1) bin2)
+		| otherwise = (Node num bin1 (insTree a bin2))
+
+	-- EJERCICIO 15
+	creaTree :: [Int] -> BinTreeInt
+
+	creaTree [] = Void
+	creaTree (x:xs) = (insTree x (creaTree xs))
+
+	-- EJERCICIO 16
+	treeElem :: Int -> BinTreeInt -> Bool
+
+	treeElem a Void = False
+	treeElem a (Node num bin1 bin2)
+		| (a == num) = True
+		| (a < num) = (treeElem a bin1)
+		| otherwise = (treeElem a bin2)
+
+
+	-- AMPLIACIÓN
+
+	-- EJERCICIO 21 
+	dupElem :: BinTreeInt -> BinTreeInt
+
+	dupElem Void = Void
+	dupElem (Node num bin1 bin2) = (Node (2*num) (dupElem bin1) (dupElem bin2))
--- HASKELL/P2/tree.hs
+++ HASKELL/P2/tree.hs
@@ -5,13 +5,16 @@ module Tree where
 	-- EJERCICIO 12
 	symmetric :: Tree a -> Tree a
 
-	symmetric (Leaf a) = (Leaf a)
-	symmetric (Branch (Leaf a) (Leaf b)) = Branch (Leaf b) (Leaf a) -- swap de ojas
-	symmetric (Branch (Leaf a) (Branch b b1)) = Branch (symmetric (Branch b b1)) (Leaf a) -- swap de oja y branch (llamada a recursión)
-	symmetric (Branch (Branch a a1) (Leaf b)) = Branch (Leaf b) (symmetric (Branch a a1)) -- swap de branch (recur) y oja
-	symmetric (Branch (Branch a a1) (Branch b b1)) = Branch (symmetric (Branch b b1)) (symmetric (Branch a a1)) -- swap de branches y llamada doble a recursión
+	{- Traza
+	symmetric' (Leaf a) = (Leaf a)
+	symmetric' (Branch (Leaf a) (Leaf b)) = Branch (Leaf b) (Leaf a) -- swap de ojas
+	symmetric' (Branch (Leaf a) (Branch b b1)) = Branch (symmetric (Branch b b1)) (Leaf a) -- swap de oja y branch (llamada a recursión)
+	symmetric' (Branch (Branch a a1) (Leaf b)) = Branch (Leaf b) (symmetric (Branch a a1)) -- swap de branch (recur) y oja
+	symmetric' (Branch (Branch a a1) (Branch b b1)) = Branch (symmetric (Branch b b1)) (symmetric (Branch a a1)) -- swap de branches y llamada doble a recursión
+	-}
 
-
+	symmetric (Leaf a )= (Leaf a)
+	symmetric (Branch a b) = Branch (symmetric b) (symmetric a) 
 
 	-- EJERCICIO 13
 	listToTree :: [a] -> Tree a
@@ -25,3 +28,26 @@ module Tree where
 
 	treeToList (Leaf a) = [a]
 	treeToList (Branch a b) = (treeToList a) ++ (treeToList b)
+
+
+	-- AMPLIACIÓN
+
+	-- EJERCICIO 22     funcion  ->  Arbol   -> num
+
+	data Tree' a = Branch' a (Tree' a) (Tree' a) | Void' deriving Show
+
+	countProperty :: (a -> Bool) -> (Tree' a) -> Int
+	countProperty func Void' = 0;
+	countProperty func (Branch' num t1 t2)
+		| (func num) = 1 + (countProperty func t1) + (countProperty func t2)
+		| otherwise = 0 + (countProperty func t1) + (countProperty func t2)
+
+
+
+
+
+
+
+
+
+
